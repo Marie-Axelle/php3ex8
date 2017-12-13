d8b16ba9807e1a6ed90c30c54a0796c8bc68bac4
Marie-Axelle Simoes <Vous@exemple.com>
2017-12-13 11:43:25 +0100

    boucle 8

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>boucle</title>
+  </head>
+  <body>
+    <?php
+  for ($Count=200; $Count >=0 ; $Count-= 12) {
+    # incrémentation par pas de 15 jusqu'à 100
+    echo 'Enfin !!!! '. $Count. '<br/>';
+  }
+     ?>
+  </body>
+</html>
